7a8894d9535fe026728022204615cb05f9468e8c
Joe Manthey <jdmanthey@gmail.com>
2024-04-01 21:07:04 -0500

    Create 00_setup.sh

---
+++ 00_setup.sh
@@ -0,0 +1,17 @@
+# move to working directory
+# 00_fastq directory already there with all the raw data files in *fastq.gz format
+
+# reference genome already indexed from previous study
+
+# make directories for organization during analyses
+mkdir 01_cleaned
+mkdir 01_mtDNA
+mkdir 01_blochmannia
+mkdir 01_bam_files
+mkdir 02_vcf
+mkdir 03_vcf
+mkdir 04_vcf
+mkdir 05_filtered_vcf_100kbp
+mkdir 05_filtered_vcf_100kbp/windows
+mkdir 10_align_script
+
