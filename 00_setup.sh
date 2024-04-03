9397da5ffe14842950c2e5826e2cfc1ba8afea95
Joe Manthey <jdmanthey@gmail.com>
2024-04-03 11:24:32 -0500

    Update 00_setup.sh

--- 00_setup.sh
+++ 00_setup.sh
@@ -13,5 +13,8 @@ mkdir 03_vcf
 mkdir 04_vcf
 mkdir 05_filtered_vcf_100kbp
 mkdir 05_filtered_vcf_100kbp/windows
+mkdir 19_minys
 mkdir 20_align_script
 mkdir 21_genotype_script
+mkdir 22_filter_script
+mkdir 23_minys_script
