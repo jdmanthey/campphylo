5682ad73c1b0835221e82b001aadfe9971aba527
Joe Manthey <jdmanthey@gmail.com>
2024-04-03 11:37:39 -0500

    Create 03_merge_vcf.sh

---
+++ 03_merge_vcf.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=merge
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --partition=nocona
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-31
+
+source activate bcftools
+
+# define main working directory
+workdir=/lustre/scratch/jmanthey/09_antphylo
+
+# define variables
+region_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/scaffolds.txt | tail -n1 )
+
+# run bcftools to merge the vcf files
+bcftools merge -m id --regions ${region_array} ${workdir}/03_vcf/*vcf.gz > ${workdir}/04_vcf/${region_array}.vcf
