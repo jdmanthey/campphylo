bcf267a65a19820cbf1d9d9d1ef74d37e5702157
Joe Manthey <jdmanthey@gmail.com>
2024-04-10 18:03:07 -0500

    Update 08_twisst.sh

--- 08_twisst.sh
+++ 08_twisst.sh
@@ -10,16 +10,10 @@ python /home/jmanthey/twisst/twisst.py -t camponotus_100kbp_pruned.trees.gz -w o
 -g modoc_east -g modoc_west -g new -g herculeanus -g novaeboracencis -g chromaiodes -g pennsylvanicus \
 -g laevissimus --outgroup laevissimus --method complete --groupsFile twisst_groups.txt
 
-#!/bin/sh
-#SBATCH --chdir=./
-#SBATCH --job-name=twisst2
-#SBATCH --nodes=1 --ntasks=2
-#SBATCH --partition quanah
-#SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=8G
 
 python /home/jmanthey/twisst/twisst.py -t camponotus_100kbp_pruned.trees.gz -w output2.weights.csv.gz --outputTopos output_topologies2.trees \
 -g herc_group -g penn_group -g new \
 -g laevissimus --outgroup laevissimus --method complete --groupsFile twisst_groups2.txt
 
-
+python /home/jmanthey/twisst/twisst.py -t camponotus_100kbp_hercgroup.trees.gz -w output_herc.weights.csv.gz --outputTopos output_herc_topologies.trees \
+-g modoc_east -g modoc_west -g herculeanus -g novaeboracencis --method complete --groupsFile twisst_groups_herc.txt
