e7d02dd1c4bfcc30d7d7b8e3525788112a59778a
Joe Manthey <jdmanthey@gmail.com>
2025-08-20 11:26:33 -0500

    Update 00_setup.sh

--- 00_setup.sh
+++ 00_setup.sh
@@ -14,10 +14,21 @@ mkdir 03_contam
 mkdir 04_vcf
 mkdir 05_filtered_vcf
 mkdir 05_filtered_vcf/windows
-mkdir 19_minys
-mkdir 20_align_genotype_script
-mkdir 21_merge_script
-mkdir 22_filter_script
-mkdir 23_minys_script
+mkdir 06_divergence
+mkdir 07_mcmctree
+mkdir 08_gene_flow
+mkdir 09_ils
+mkdir 10_eems
+
+mkdir 20_bloch_vcf
+mkdir 21_bloch_vcf
+mkdir 22_bloch_vcf_filtered
+mkdir 23_bloch_phylo
+mkdir 24_minys
+mkdir 25_gapseq
+
+mkdir 30_align_genotype_script
+mkdir 31_merge_script
+mkdir 32_filter_script
 
 # copy the contam_check.r script to the 20_align_genotype_script directory
