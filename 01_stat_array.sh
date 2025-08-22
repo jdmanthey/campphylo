b0543700ad41574950a5dbc32489c79b1675d8ea
Joe Manthey <jdmanthey@gmail.com>
2025-08-22 16:47:07 -0500

    Add files via upload

---
+++ 01_stat_array.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=stats
+#SBATCH --nodes=1 --ntasks=2
+#SBATCH --partition nocona
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-750
+
+source activate bcftools
+
+# Set the number of runs that each SLURM task should do
+PER_TASK=8
+
+# Calculate the starting and ending values for this task based
+# on the SLURM task and the number of runs per task.
+START_NUM=$(( ($SLURM_ARRAY_TASK_ID - 1) * $PER_TASK + 1 ))
+END_NUM=$(( $SLURM_ARRAY_TASK_ID * $PER_TASK ))
+
+# Print the task and run range
+echo This is task $SLURM_ARRAY_TASK_ID, which will do runs $START_NUM to $END_NUM
+
+# Run the loop of runs for this task.
+for (( run=$START_NUM; run<=$END_NUM; run++ )); do
+	echo This is SLURM task $SLURM_ARRAY_TASK_ID, run number $run
+
+	chrom_array=$( head -n${run} stat_helper.txt | cut -f1 | tail -n1 )
+
+	start_array=$( head -n${run} stat_helper.txt | cut -f2 | tail -n1 )
+
+	end_array=$( head -n${run} stat_helper.txt | cut -f3 | tail -n1 )
+
+	gunzip -cd /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/scaffold0031.recode.vcf.gz | grep "#" > /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf
+
+	tabix /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/${chrom_array}.recode.vcf.gz ${chrom_array}:${start_array}-${end_array} >> /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf
+
+	Rscript _window_stat_calculations.r /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf popmap_stats.txt
+
+	rm /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf
+	raxmlHPC-PTHREADS-SSE3 -T 2 -f a -x 50 -m GTRCAT -p 253 -N 2 -s /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/${chrom_array}__${start_array}__${end_array}.fasta -n ${chrom_array}__${start_array}__${end_array}.tre -w /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/
+
+	rm /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/${chrom_array}__${start_array}__${end_array}.fasta
+	rm /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/RAxML_bestTree.${chrom_array}__${start_array}__${end_array}.tre
+	rm /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/RAxML_bipartitionsBranchLabels.${chrom_array}__${start_array}__${end_array}.tre
+	rm /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/RAxML_info.${chrom_array}__${start_array}__${end_array}.tre
+	rm /lustre/scratch/jmanthey/08_ant_phylo/05_filtered_vcf/windows/RAxML_bootstrap.${chrom_array}__${start_array}__${end_array}.tre
+
+done
