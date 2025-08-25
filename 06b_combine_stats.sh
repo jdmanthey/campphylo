7df3c512e9a6e522d5bd30a1bfee66d85c77a8a8
Joe Manthey <jdmanthey@gmail.com>
2025-08-25 09:41:18 -0500

    Update 06b_combine_stats.sh

--- 06b_combine_stats.sh
+++ 06b_combine_stats.sh
@@ -1,13 +1,8 @@
-cd /lustre/scratch/jmanthey/09_antphylo/05_filtered_vcf/windows
+cd /lustre/scratch/jmanthey/08_ant_phylo/33_stats
 
-# combine the output for different analyses into a single file each
+# combine the output for different windows into a single file 
 # first add a header for each file
-grep 'pop1' scaffold0001__100001__200000__stats.txt > ../window_heterozygosity.txt
-grep 'pop1' scaffold0001__100001__200000__stats.txt > ../window_Dxy.txt
-grep 'pop1' scaffold0001__100001__200000__stats.txt > ../window_Fst.txt
+grep 'pop1' scaffold0001__10000001__10050000__stats.txt > ../window_stats.txt
 
-# add the relevant stats to each file
-for i in $( ls *stats.txt ); do grep 'heterozygosity' $i >> ../window_heterozygosity.txt; done
-for i in $( ls *stats.txt ); do grep 'Dxy' $i >> ../window_Dxy.txt; done
-for i in $( ls *stats.txt ); do grep 'Fst' $i >> ../window_Fst.txt; done
-
+# add the stats to the file
+for i in $( ls *stats.txt ); do grep -v 'pop1' $i >> ../window_stats.txt; done
