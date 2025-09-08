be914698249f4a8ff72777445e73a0ecffbe4e50
Joe Manthey <jdmanthey@gmail.com>
2025-09-08 10:41:59 -0500

    Update 10_merge_filter_blochmannia.sh

--- 10_merge_filter_blochmannia.sh
+++ 10_merge_filter_blochmannia.sh
@@ -17,15 +17,8 @@ bcftools merge ${workdir}/01_blochmannia/*filtered.vcf.gz > \
 ${workdir}/01_blochmannia/total.vcf
 
 # filter based on missing data for phylogenetics
-vcftools --vcf ${workdir}/01_blochmannia/total.vcf --keep bloch.txt --max-missing 1.0 \
---max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
---out ${workdir}/01_blochmannia/total_filtered1.vcf
-
-vcftools --vcf ${workdir}/01_blochmannia/total.vcf --keep bloch.txt --max-missing 0.95 \
---max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
---out ${workdir}/01_blochmannia/total_filtered2.vcf
 
 vcftools --vcf ${workdir}/01_blochmannia/total.vcf --keep bloch.txt --max-missing 0.9 \
 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
---out ${workdir}/01_blochmannia/total_filtered3.vcf
+--out ${workdir}/01_blochmannia/total_filtered.vcf
 
