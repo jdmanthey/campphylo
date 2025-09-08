b67fcb22741979ba6d25b55c00af69704cabd375
Joe Manthey <jdmanthey@gmail.com>
2025-09-08 11:15:00 -0500

    Create 12_raxml_blochmannia.sh

---
+++ 12_raxml_blochmannia.sh
@@ -0,0 +1,5 @@
+raxmlHPC-PTHREADS-SSE3 -T 4 -f a -x 50 -m GTRCAT -p 253 -N 100 \
+-s /lustre/scratch/jmanthey/08_ant_phylo/01_blochmannia/blochmannia.fasta \
+-n blochmannia.tre \
+-w /lustre/scratch/jmanthey/08_ant_phylo/
+
