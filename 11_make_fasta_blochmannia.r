6f0da31d0e578d8721fbb7c372a9632be3d01caa
Joe Manthey <jdmanthey@gmail.com>
2025-09-08 10:28:55 -0500

    Update 11_make_fasta_blochmannia.r

--- 11_make_fasta_blochmannia.r
+++ 11_make_fasta_blochmannia.r
@@ -11,38 +11,15 @@ create_fasta_from_vcf <- function(xxx, outname, num_sites_needed) {
 		# define names of individuals in output fasta
 		output_names_fasta <- paste(">", colnames(genotypes), sep="")
 		
-		# define heterozygous ambiguities
-    	het <- rep("N", nrow(allele_info))
-    	het[allele_info[,1] == "A" & allele_info[,2] == "C"] <- "M"
-     	het[allele_info[,1] == "A" & allele_info[,2] == "G"] <- "R"
-      	het[allele_info[,1] == "A" & allele_info[,2] == "T"] <- "W"
-       	het[allele_info[,1] == "C" & allele_info[,2] == "A"] <- "M"
-       	het[allele_info[,1] == "C" & allele_info[,2] == "G"] <- "S"
-       	het[allele_info[,1] == "C" & allele_info[,2] == "T"] <- "Y"
-       	het[allele_info[,1] == "G" & allele_info[,2] == "A"] <- "R"
-       	het[allele_info[,1] == "G" & allele_info[,2] == "C"] <- "S"
-       	het[allele_info[,1] == "G" & allele_info[,2] == "T"] <- "K"
-       	het[allele_info[,1] == "T" & allele_info[,2] == "A"] <- "W"
-       	het[allele_info[,1] == "T" & allele_info[,2] == "C"] <- "Y"
-       	het[allele_info[,1] == "T" & allele_info[,2] == "G"] <- "K"
-       	
+
        	# convert all numbers in genotypes to actual bases and ambiguities		
 		for(a in 1:ncol(genotypes)) {
 			# keep only genotype
-			genotypes[,a] <- substr(genotypes[,a], 1, 3)
+			genotypes[,a] <- substr(genotypes[,a], 1, 1)
 			# convert to bases
-			genotypes[,a][genotypes[,a] == "0/0"] <- allele_info[genotypes[,a] == "0/0",1]
-			genotypes[,a][genotypes[,a] == "1/1"] <- allele_info[genotypes[,a] == "1/1",2]
-			genotypes[,a][genotypes[,a] == "./."] <- "?"
-			genotypes[,a][genotypes[,a] == "0/1"] <- het[genotypes[,a] == "0/1"]
-			# choose random allele for heterozygotes
-			genotypes[,a][genotypes[,a] == "R"] <- sample(c("A", "G"), length(genotypes[,a][genotypes[,a] == "R"]), replace=T)
-			genotypes[,a][genotypes[,a] == "K"] <- sample(c("T", "G"), length(genotypes[,a][genotypes[,a] == "K"]), replace=T)
-			genotypes[,a][genotypes[,a] == "S"] <- sample(c("C", "G"), length(genotypes[,a][genotypes[,a] == "S"]), replace=T)
-			genotypes[,a][genotypes[,a] == "Y"] <- sample(c("C", "T"), length(genotypes[,a][genotypes[,a] == "Y"]), replace=T)
-			genotypes[,a][genotypes[,a] == "M"] <- sample(c("A", "C"), length(genotypes[,a][genotypes[,a] == "M"]), replace=T)
-			genotypes[,a][genotypes[,a] == "W"] <- sample(c("A", "T"), length(genotypes[,a][genotypes[,a] == "W"]), replace=T)
-
+			genotypes[,a][genotypes[,a] == "0"] <- allele_info[genotypes[,a] == "0",1]
+			genotypes[,a][genotypes[,a] == "1"] <- allele_info[genotypes[,a] == "1",2]
+			genotypes[,a][genotypes[,a] == "."] <- "?"
 		}
     
 		# write output
