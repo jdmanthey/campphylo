e901c74ec58483eaa6490271af97b766eae6c90d
Joe Manthey <jdmanthey@gmail.com>
2025-09-12 09:44:09 -0500

    Create 06_astral_of_mrbayes_trees.sh

---
+++ 14_snaq/06_astral_of_mrbayes_trees.sh
@@ -0,0 +1,11 @@
+
+
+java -jar ~/Astral/astral.5.6.3.jar -i camponotus_mrbayes.trees \
+--namemapfile astral_focal_map.txt -o camponotus_focal_astral.tre 2> camponotus_focal_astral.log
+
+
+java -jar ~/Astral/astral.5.6.3.jar -i camponotus_mrbayes.trees \
+-o camponotus_focal_inds_astral.tre 2> camponotus_focal_inds_astral.log
+
+
+
